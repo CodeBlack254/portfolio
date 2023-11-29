daf0eb2756b751abef0d3ce5d613c5407dc81091
CodeBlack254 <deriqrose@gmail.com>
2023-11-29 11:20:28 +0300

    Home page done

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info">
+        <h1> Mobile Applications | Web Applications | RESTFUL API's</h1>
+        <Typed
+            className = "type-text"
+            strings = {["Web Design", "Web Development", "Mobile Application Development", "REST API Development"]}
+            typeSpeed={40}
+            backSpeed={60}
+            loop
+        />
+        <a href="#" className="btn-main-offer">contact me </a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
